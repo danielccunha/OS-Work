e6b25aed283c13a53918ccf45c2a0ed6a4a76dc7
Daniel Cunha <danielcunha54@gmail.com>
2018-09-17 17:35:05 -0300

    Created model Queue.c and initialized threadGenerator method

--- program.c
+++ program.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
 #include "Models/Control.c"
-#include "Models/Stack.c"
+#include "Models/Queue.c"
 
 struct Control ctrl;
-struct Stack* stack;
+Queue* queue;
+
+void *threadCPU(void *argv);
+void *threadGerador(void *args);
+void *createThread(void *args);
 
 int main(int argc, char const *argv[])
 {
@@ -13,10 +20,48 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    ctrl = getControl(argv);
-    stack = createStack(5);
+    srand(time(NULL));
 
+    pthread_t tidCPU;
+    pthread_create(&tidCPU, NULL, threadCPU, argv);
+    pthread_join(tidCPU, NULL);
     
-
     return 0;
+}
+
+void *threadCPU(void *args) 
+{
+    ctrl = getControl(args);
+    queue = ConstructQueue(ctrl.T);
+
+    pthread_t tidGerador;
+    pthread_create(&tidGerador, NULL, threadGerador, NULL);
+    pthread_join(tidGerador, NULL);
+
+    return NULL;
+}
+
+void *threadGerador(void *args)
+{
+    pthread_t tid[ctrl.N];
+
+    for(int i = 0; i < ctrl.N; ++i)
+    {
+        // TODO: If queue is not full, enqueue another thread
+        pthread_create(&tid[i], NULL, createThread, NULL);
+
+        NODE *pN = (NODE*) malloc(sizeof (NODE));
+        pN->data.info = tid[i];
+        
+        Enqueue(queue, pN);
+    }
+    
+    return NULL;
+}
+
+void *createThread(void *args)
+{
+    // TODO: Create thread
+    printf("Created thread...\n");
+    return NULL;
 }
